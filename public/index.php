15c99a2ff8abd6551d1f9451c9d3036d1f0ed6de
Micha Alt <php@lean-stack.de>
2018-09-11 23:42:48 +0200

    Preparing parameter injections.

--- public/index.php
+++ public/index.php
@@ -13,5 +13,13 @@ if (method_exists($ctrl, 'setContainer')) {
     call_user_func([$ctrl, 'setContainer'], $container);
 }
 
+$action = 'index';
+
+$reflectionParams = (new ReflectionObject($ctrl))->getMethod($action)->getParameters();
+foreach ($reflectionParams as $param) {
+    // Preparing parameter injections
+    var_dump($param->getType()->getName());
+}
+
 $response = $ctrl->index();
 $response->send();
