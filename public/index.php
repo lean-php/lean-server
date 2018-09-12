24cc843bf691883c6e994fb54e11bf18193d608d
Micha Alt <php@lean-stack.de>
2018-09-12 08:04:39 +0200

    Routing library fastroute installed.

--- public/index.php
+++ public/index.php
@@ -2,18 +2,43 @@
 
 require '../vendor/autoload.php';
 
+const IS_DEBUG_ENABLED = true;
+
+//
+// Configure Container
+//
 $builder = new DI\ContainerBuilder();
 $builder->addDefinitions([
     League\Plates\Engine::class => DI\create()->constructor('../templates', 'phtml')
 ]);
 $container = $builder->build();
 
-$ctrl = $container->get(\App\Controller\DefaultController::class);
+//
+// Configure Routing
+//
+$dispatcher = FastRoute\cachedDispatcher(function(FastRoute\RouteCollector $r) {
+    // Thirs param as you like: string, array, object
+    $r->addRoute('GET', '/', [\App\Controller\DefaultController::class, 'index']);
+    $r->addRoute('GET', '/{rolle}', [\App\Controller\DefaultController::class, 'index']);
+    $r->addRoute('GET', '/{rolle}/kommune', [\App\Controller\DefaultController::class, 'index']);
+    $r->addRoute('GET', '/{rolle}/kommune/{ags}/{controller}[/{action}]', [\App\Controller\DefaultController::class, 'index']);
+}, [
+    'cacheFile' => dirname(__DIR__) . '/var/route.cache', /* required */
+    'cacheDisabled' => IS_DEBUG_ENABLED,     /* optional, enabled by default */
+]);
+
+//
+// Request/Response Cycle
+//
+$route = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+var_dump($route); exit();
+
+$ctrl = $container->get($route[1][0]);
 if (method_exists($ctrl, 'setContainer')) {
     call_user_func([$ctrl, 'setContainer'], $container);
 }
 
-$action = 'index';
+$action = $route[1][1];
 
 $reflectionParams = (new ReflectionObject($ctrl))->getMethod($action)->getParameters();
 foreach ($reflectionParams as $param) {
@@ -21,5 +46,5 @@ foreach ($reflectionParams as $param) {
     var_dump($param->getType()->getName());
 }
 
-$response = $ctrl->index();
+$response = call_user_func([$ctrl, $action], []);
 $response->send();
